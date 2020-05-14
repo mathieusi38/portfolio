b0219c89b4ba4a7598f556cb02377e9dd971cd9d
mathieusi38 <60643534+mathieusi38@users.noreply.github.com>
2020-05-14 21:39:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 $('#openNav').on('click', function() {
   $('#myNav').css("width", "100%");
+  $('.embed-responsive-item').attr({'autoplay':'true'});
 });
 
 $('#closeNav').on('click', function() {
   $('#myNav').css("width", "0%");
+  $('.embed-responsive-item').attr({'autoplay':'false'});
 });
